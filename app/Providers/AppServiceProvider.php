b2e4c102bed5d7303dc49ae45ab6f6bf925547f9
Iber <iber65525526@gmail.com>
2021-05-16 16:59:39 -0400

    quitando heroku ggg

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,18 +11,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    
-    public function boot(UrlGenerator $url)
-    {
-    if (env('REDIRECT_HTTPS')) {
-        $url->formatScheme('https://');
-    }
-    }
     public function register()
     {
-        if (env('REDIRECT_HTTPS')) {
-            $this->app['request']->server->set('HTTPS', true);
-        }
+        //
     }
 
     /**
